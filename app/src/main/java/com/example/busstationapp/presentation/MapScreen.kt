8336afa70c0709479616b99f7ddbd548e209981e
pc <aydeniztalha7@gmail.com>
2023-10-01 03:02:43 +0300

    Updated dependencies

---
+++ app/src/main/java/com/example/busstationapp/presentation/MapScreen.kt
@@ -0,0 +1,11 @@
+package com.example.busstationapp.presentation
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun MapScreen(
+    viewModel: MapsViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
+
+}
